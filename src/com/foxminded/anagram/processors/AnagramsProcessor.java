2f8c8c87c8ddabbd216a63b299abbc052993ebfc
Лебедев Александр Сергеевич <mrle080693@gmail.com>
2019-08-14 14:07:10 +0300

    Refact local variables names and changing input parametres

--- src/com/foxminded/anagram/processors/AnagramsProcessor.java
+++ src/com/foxminded/anagram/processors/AnagramsProcessor.java
@@ -2,40 +2,43 @@ package com.foxminded.anagram.processors;
 
 public class AnagramsProcessor {
     private final String SEPARATOR = " ";
-    private final String NON_LETTERS = "[^\\p{L}\\s]";
+    private final String NON_LETTERS_PATTERN = "[^\\p{L}\\s]";
 
-    public String process(String userInput){
+    public String process(String input){
         String result = "";
-        if(userInput != null) {
-            String[] splitedInput = userInput.split(SEPARATOR);
+        if(input != null) {
+            String[] splitedInput = input.split(SEPARATOR);
             splitedInput = deleteSymbols(splitedInput);
             result = reverseInput(splitedInput);
-            result = insertSymbols(userInput, result);
+            result = insertSymbols(input, result);
         }
         return result;
     }
 
-    private String[] deleteSymbols(String[] splitedInput) {
-        for (int i = 0; i <= splitedInput.length - 1; i++) {
-            splitedInput[i] = splitedInput[i].replaceAll(NON_LETTERS, "");
+    private String[] deleteSymbols(String[] input) {
+        String[] result = new String[input.length];
+        for (int i = 0; i <= input.length - 1; i++) {
+            result[i] = input[i].replaceAll(NON_LETTERS_PATTERN, "");
         }
-        return splitedInput;
+        return result;
     }
 
-    private String reverseInput(String[] splitedInput){
+    private String reverseInput(String[] input){
+        String[] reversedInput = new String[input.length];
         String result = "";
-        for(int i = 0; i <= splitedInput.length - 1; i++) {
-            splitedInput[i] = new StringBuffer(splitedInput[i]).reverse().toString();
-            result = result + splitedInput[i] + SEPARATOR;
+        for(int i = 0; i <= input.length - 1; i++) {
+            reversedInput[i] = new StringBuffer(input[i]).reverse().toString();
+            result = result + reversedInput[i] + SEPARATOR;
         }
         return result;
     }
 
-    private String insertSymbols(String userInput, String result) {
-        String[] inputAsArray = new String[userInput.length()];
-        for (int i = 0; i <= userInput.length() - 1; i++) {
-            inputAsArray[i] = userInput.substring(i, i + 1);
-            if (inputAsArray[i].matches(NON_LETTERS)) {
+    private String insertSymbols(String input, String inputOnlyLetters) {
+        String[] inputAsArray = new String[input.length()];
+        String result = inputOnlyLetters;
+        for (int i = 0; i <= input.length() - 1; i++) {
+            inputAsArray[i] = input.substring(i, i + 1);
+            if (inputAsArray[i].matches(NON_LETTERS_PATTERN)) {
                 result = result.substring(0, i) + inputAsArray[i] + result.substring(i);
             }
         }
